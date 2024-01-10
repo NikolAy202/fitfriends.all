354bdb216c00ff39cff2f866f121939d1f00fed8
Николай <rukin.nik.1996@mail.ru>
2024-01-10 22:31:24 +0700

    main

--- project/apps/bff/src/app/app.config.ts
+++ project/apps/bff/src/app/app.config.ts
@@ -1,13 +1,13 @@
 export enum ApplicationServiceURL {
-  Traning = 'http://fitfriends.traning:3004/api/traning',
-  Orders = 'http://fitfriends.traning:3004/api/orders',
-  Auth = 'http://fitfriends.users:5000/api/auth',
+  Traning = 'http://localhost:3004/api/traning',
+  Orders = 'http://localhost:3004/api/orders',
+  Auth = 'http://localhost:5000/api/auth',
   Users = 'http://localhost:5000/api/users',
   Notify = 'http://localhost:6000/api/notify',
-  Request = 'http://fitfriends.users:5000/api/request',
-  Comment = 'http://fitfriends.traning:3004/api/comment',
-  Uploads = 'http://fitfriends.uploade:3001/api/files/upload',
-  Files = 'http://fitfriends.uploade:3001/api/files',
+  Request = 'http://localhost:5000/api/request',
+  Comment = 'http://localhost:3004/api/comment',
+  Uploads = 'http://localhost:3001/api/files/upload',
+  Files = 'http://localhost:3001/api/files',
 }
 
 export const HTTP_CLIENT_MAX_REDIRECTS = 5;
